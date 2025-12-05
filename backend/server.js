9291140ea06029eaf50b1e7e1ed8d689a8386cb4
shikha033 <shikhasingh242494@gmail.com>
2025-12-05 21:41:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,11 @@ require("dotenv").config();
 const express = require("express");
 // express app
 const app = express();
+// middleware
+app.use((req,res,next) => {
+   console.log(req.path,req.method )
+   next()
+})
 // routes---
 app.get("/", (req, res) => {
   res.json({ mssg: "Welcome to the app" });
