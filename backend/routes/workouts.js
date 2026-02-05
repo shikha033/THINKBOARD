cf40cee6fa00089e22c123c2bf03ded96e579acf
shikha033 <shikhasingh242494@gmail.com>
2026-02-05 23:10:56 +0530

    Update workouts.js

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,8 +2,16 @@ const express= require('express')
 
 const router = require.Router()
 
-router.get('/', (req,res) => {})
+//GET all workouts
+router.get('/', (req,res) => {
+    res.json({mssg: 'GET all workouts'})
+})
 
+
+// GET a single workout
+router.get('/:id', (req,res) => {
+    res.json({mssg: 'GET a single workout'})
+}
 
 
 
